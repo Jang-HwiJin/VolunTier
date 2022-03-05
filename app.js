76bee85c1dae8da36000501ad29b24fae5eb88e0
David <davidjang01@gmail.com>
2022-03-05 17:55:22 -0600

    Update everything

--- app.js
+++ app.js
@@ -14,6 +14,26 @@ app.get("/", function(req, res) {
   res.render("home");
 });
 
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.get("/organization", function(req, res) {
+  res.render("organization");
+});
+
+app.get("/profile", function(req, res) {
+  res.render("profile");
+});
+
+app.get("/volunteer", function(req, res) {
+  res.render("volunteer");
+});
+
+app.get("/map", function(req, res) {
+  res.render("map");
+});
+
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
